a606837ead0794d124ae4535b56a2391888c88e8
Baptiste Lecocq <hello@tiste.io>
2017-07-21 17:11:31 +0200

    feat: add status check name

--- app.rb
+++ app.rb
@@ -17,6 +17,8 @@ post '/event_handler' do
 
   case request.env['HTTP_X_GITHUB_EVENT']
   when 'pull_request'
+    puts @payload['action']
+
     if @payload['action'] === 'labeled'
       process_pull_request(@payload['pull_request'], @payload['label'], true)
     end
@@ -24,15 +26,25 @@ post '/event_handler' do
     if @payload['action'] === 'unlabeled'
       process_pull_request(@payload['pull_request'], @payload['label'], false)
     end
+
+    if @payload['action'] === 'opened'
+      process_pull_request(@payload['pull_request'], {}, false)
+    end
   end
 end
 
 helpers do
-  def process_pull_request(pull_request, label, is_valid)
-    if label['name'] === 'Mergeable'
-      state = is_valid ? 'success' : 'error'
+  def process_pull_request(pull_request, label, is_success)
+    do_something = label['name'] === 'Mergeable' || label.empty?
 
-      @client.create_status(pull_request['base']['repo']['full_name'], pull_request['head']['sha'], state)
+    if do_something
+      state = is_success ? 'success' : 'error'
+      description = is_success ? 'The mergeable label is attached, go for it' : 'Pull Request doesn\'t seem to be mergeable yet'
+
+      @client.create_status(pull_request['base']['repo']['full_name'], pull_request['head']['sha'], state, {
+        context: 'label-status-check',
+        description: description,
+      })
     end
   end
 end
