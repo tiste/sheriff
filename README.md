45c5e0b7af98c0602d60cb1389fb33cb561d3b78
Baptiste Lecocq <hello@tiste.io>
2017-07-25 16:18:18 +0200

    feat: add the reviewers check

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Sheriff
+<p align="center">
+  <img src="http://ccofal.org/alabama/images/Alabama-Sheriffs.png" alt="Sheriff">
+</p>
 
 > "Don't merge that pull request because I'm not okay with it. You listen to me? Uh..."
 >
@@ -6,9 +8,11 @@
 
 ## Install
 
-Just add a new webhook to your Github repo, then give it "Pull Request" and "Issues" events.
+Just add a new webhook to your Github repo, then give it "Pull Request", "Pull request review" and "Issues" events.
 
 Base URL: http://sheriff.tiste.io
+
+Example: http://sheriff.tiste.io/reviews?minimum=3
 
 ## How it works?
 
@@ -17,3 +21,9 @@ Base URL: http://sheriff.tiste.io
 When adding a "Mergeable" label to your pull request, it will pass the status check to green, otherwise red.
 
 > You can configure the label name with the query param `?name=AnotherLabel`
+
+### /reviews
+
+When 2 or more approvals are given to your pull request, it will pass the status check to green, otherwise red.
+
+> You can configure the minimum number of reviews with the query param `?minimum=1`
--- app.rb
+++ app.rb
@@ -14,38 +14,69 @@ end
 
 post '/label' do
   @payload = JSON.parse(params[:payload])
-  puts "Action: #{@payload['action']}"
+  puts "Label: #{@payload['action']}"
 
   label_name = (params[:name] || 'mergeable').to_s.downcase
 
   case request.env['HTTP_X_GITHUB_EVENT']
   when 'pull_request'
     if @payload['action'] === 'labeled'
-      process_pull_request(@payload['pull_request'], @payload['label'], label_name, true)
+      process_label(@payload['pull_request'], @payload['label'], label_name, true)
     end
 
     if @payload['action'] === 'unlabeled'
-      process_pull_request(@payload['pull_request'], @payload['label'], label_name, false)
+      process_label(@payload['pull_request'], @payload['label'], label_name, false)
     end
 
     if @payload['action'] === 'opened'
-      process_pull_request(@payload['pull_request'], {}, label_name, false)
+      process_label(@payload['pull_request'], {}, label_name, false)
+    end
+  end
+end
+
+post '/reviews' do
+  @payload = JSON.parse(params[:payload])
+  puts "Reviews: #{@payload['action']}"
+
+  minimum = Integer(params[:minimum]) rescue 2
+
+  case request.env['HTTP_X_GITHUB_EVENT']
+  when 'pull_request'
+    if @payload['action'] === 'opened'
+      process_reviews(@payload['pull_request'], minimum)
+    end
+  when 'pull_request_review'
+    if @payload['action'] === 'submitted'
+      process_reviews(@payload['pull_request'], minimum)
     end
   end
 end
 
 helpers do
-  def process_pull_request(pull_request, label, name, is_success)
+  def process_label(pull_request, label, name, is_success)
     do_something = label['name'].to_s.downcase === name || label.empty?
 
     if do_something
       state = is_success ? 'success' : 'error'
-      description = is_success ? "The #{name} label is attached, go for it" : "Pull Request doesn\'t have the label #{name} yet"
+      description = is_success ? "The \"#{name}\" label is attached, go for it" : "Pull Request doesn\'t have the label \"#{name}\" yet"
 
       @client.create_status(pull_request['base']['repo']['full_name'], pull_request['head']['sha'], state, {
         context: 'sheriff/label',
         description: description,
       })
     end
+  end
+
+  def process_reviews(pull_request, minimum)
+    reviews = @client.pull_request_reviews(pull_request['base']['repo']['full_name'], pull_request['number'])
+
+    is_success = reviews.sort_by { |review| review[:id] }.reverse!.uniq { |review| review[:user][:id] }.select { |review| review[:state] === 'APPROVED' }.size >= minimum
+    state = is_success ? 'success' : 'error'
+    description = is_success ? "There is at least #{minimum} or more approvals, it's okay" : "Pull Request doesn't have enough reviews (#{minimum})"
+
+    @client.create_status(pull_request['base']['repo']['full_name'], pull_request['head']['sha'], state, {
+      context: 'sheriff/reviews',
+      description: description,
+    })
   end
 end
