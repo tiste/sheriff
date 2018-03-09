6a67e80dd98b10758b411c9be2528718f3790e98
Baptiste Lecocq <hello@tiste.io>
2018-03-09 17:36:54 +0100

    fix: update to new vuetify semantic

--- views/FeaturePage.vue
+++ views/FeaturePage.vue
@@ -6,11 +6,13 @@
             <v-btn flat>Token: {{token}}</v-btn>
         </v-toolbar>
 
-        <v-layout justify-space-around wrap style="margin-top: -64px;">
-            <v-flex sm8>
-                <sheriff :feature="feature"></sheriff>
-            </v-flex>
-        </v-layout>
+        <v-content style="margin-top: -64px; z-index: 10;">
+            <v-layout justify-space-around wrap>
+                <v-flex sm8>
+                    <sheriff :feature="feature"></sheriff>
+                </v-flex>
+            </v-layout>
+        </v-content>
     </v-app>
 </template>
 
--- views/HomePage.vue
+++ views/HomePage.vue
@@ -6,39 +6,41 @@
             <v-btn flat v-if="token">Token: {{token}}</v-btn>
         </v-toolbar>
 
-        <v-layout justify-space-around wrap v-if="token">
-            <v-flex class="text-xs-center" v-for="(feature, i) in FEATURES" :key="i">
-                <v-btn color="primary" round large :href="i">{{i}}</v-btn>
-            </v-flex>
-        </v-layout>
-        <v-layout justify-space-around wrap v-else>
-            <v-flex sm5>
-                <v-card class="card--flex-toolbar">
-                    <v-card-title>
-                        <img src="/images/github.png" alt="Github" style="width: 150px; height: 150px;">
-                    </v-card-title>
-                    <v-card-title primary-title>
-                        <div class="headline">Github</div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn color="primary" round large href="/github/login">Log in</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-            <v-flex sm5>
-                <v-card class="card--flex-toolbar">
-                    <v-card-title>
-                        <img src="/images/gitlab.png" alt="Gitlab" style="width: 150px; height: 150px;">
-                    </v-card-title>
-                    <v-card-title primary-title>
-                        <div class="headline">Gitlab</div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn color="primary" round large href="/gitlab/login">Log in</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-layout>
+        <v-content>
+            <v-layout justify-space-around wrap v-if="token">
+                <v-flex class="text-xs-center" v-for="(feature, i) in FEATURES" :key="i">
+                    <v-btn color="primary" round large :href="i">{{i}}</v-btn>
+                </v-flex>
+            </v-layout>
+            <v-layout justify-space-around wrap v-else>
+                <v-flex sm5>
+                    <v-card class="card--flex-toolbar">
+                        <v-card-title>
+                            <img src="/images/github.png" alt="Github" style="width: 150px; height: 150px;">
+                        </v-card-title>
+                        <v-card-title primary-title>
+                            <div class="headline">Github</div>
+                        </v-card-title>
+                        <v-card-actions>
+                            <v-btn color="primary" round large href="/github/login">Log in</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+                <v-flex sm5>
+                    <v-card class="card--flex-toolbar">
+                        <v-card-title>
+                            <img src="/images/gitlab.png" alt="Gitlab" style="width: 150px; height: 150px;">
+                        </v-card-title>
+                        <v-card-title primary-title>
+                            <div class="headline">Gitlab</div>
+                        </v-card-title>
+                        <v-card-actions>
+                            <v-btn color="primary" round large href="/gitlab/login">Log in</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-content>
     </v-app>
 </template>
 
